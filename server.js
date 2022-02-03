6b26d06014f76f3969dc5f00026d6c77fb0999d2
Zack Barbari <zackb@unc.edu>
2022-02-03 14:08:28 -0500

    Another syntax error

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // Require http module
-const http = require('http');
+const http = require('http')
 // Require fs module
-const fs = require('fs');
+const fs = require('fs')
 // Require minimist module (make sure you install this one via npm).
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const minimist = require('minimist')(process.argv.slice(2))
 // Define allowed argument name 'port'.
-args["port"]
+
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.port || process.env.port || 3000;
+const port = args["port"] || process.env.port || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./www/index.html` and do some stuff with it.
@@ -44,10 +44,9 @@ fs.readFile('./www/index.html', (err, index) => {
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-    server.listen(port,() => {
-        console.log(`Server listening on port ${port}')
+    server.listen(port, () => {
+        console.log('Server listening on port ${port}')
     })
 })
-
 
 // That's it! You're all done!
